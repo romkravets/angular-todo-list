cf885d69857e238601a8aa14ffbb71c1a21b886f
romkravets <romann.kravets@gmail.com>
2019-04-21 15:04:59 +0300

    Added service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -9,7 +9,21 @@ export class TodoService {
 
   getTodos() {
     return [
-      
-    ]
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: false
+        },
+    ];
   }
 }
